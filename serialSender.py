cd13d4d633f328bfba2bc80be59cc5974668e22c
Rajavenkateshwaran Subramanian <rajavn6789@gmail.com>
2020-02-02 21:06:24 +0530

    Update serialSender.py

--- serialSender.py
+++ serialSender.py
@@ -7,13 +7,15 @@ adruinoSerial = serial.Serial("COM9", 115200)
 time.sleep(2)
 print("Adruino serial connection established")
 
+def adruinoWrite(force):
+     force = round(force, 1)
+     position = force * 1000
+     position = round(position, 1)
+     position = str(position) + "\n"
+     position = position.encode()
+     adruinoSerial.write(position)
+
 while True:
-    verticalG = (round(random.uniform(-1, 1), 1))
-    if(abs(verticalG) >= 0.3):
-        pos = verticalG * 2000 ;
-        pos = round(pos, 1)
-        pos = str(pos) + "\n"
-        pos = pos.encode()
-        print(verticalG, pos)
-        adruinoSerial.write(pos)
-        time.sleep(0.1)
+    longitudinalG = (round(random.uniform(-1, 1), 1))
+    if(abs(longitudinalG) >= 0.3):
+       adruinoWrite(longitudinalG)
