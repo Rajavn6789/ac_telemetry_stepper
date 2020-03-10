486dc6faaf80dc1e3579e1e18f8ac6cc2f2f4610
Rajavenkateshwaran Subramanian <rajavn6789@gmail.com>
2020-03-10 23:46:15 +0530

    Update serialSender.py

--- serialSender.py
+++ serialSender.py
@@ -2,14 +2,15 @@ import serial
 import time
 import random
 
-adruinoSerial = serial.Serial("COM9", 115200, timeout=1000)
+adruinoSerial = serial.Serial("/dev/cu.usbmodem14101", 38400, timeout=1000)
 time.sleep(2)
 print("Adruino serial connection established")
 posList = []
 
 def adruinoWrite(force):
      force = round(force, 1)
-     position = force * 2000
+     position = force * 10000
+     position = position * -1 # reverse direction
      position = round(position, 1)
      position = str(position) + "\n"
      position = position.encode()
@@ -17,15 +18,9 @@ def adruinoWrite(force):
      adruinoSerial.write(position)
 
 while True:
-
-     longitudinalG = (round(random.uniform(-2, 2), 1))
+     longitudinalG = (round(random.uniform(-1, 1), 1))
      posList.append(longitudinalG)
-     if len(posList) > 100000:
+     if len(posList) > 1000000:
           last = posList.pop()
           adruinoWrite(last)
           posList.clear()
-
-     # position = adruinoSerial.readline()
-     # position = position.decode()  # decode byte string into Unicode  
-     # position = position.rstrip()
-     # print(position)     
